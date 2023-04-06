e0298d03ab4ca03587828d17c4f5110bb29f625c
FSalim1 <125290573+FSalim1@users.noreply.github.com>
2023-04-06 16:43:23 +0100

    Add files via upload

---
+++ viewteacher.php
@@ -0,0 +1,185 @@
+<html>
+
+<head> <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>School Management System</title></head>
+
+        <style>
+        body {
+          font-family: Arial, Helvetica, sans-serif;
+        }
+
+        .navbar {
+          overflow: hidden;
+          background-color: #333;
+        }
+
+        .navbar a {
+          float: left;
+          font-size: 16px;
+          color: white;
+          text-align: center;
+          padding: 14px 16px;
+          text-decoration: none;
+        }
+
+        .dropdown {
+          float: left;
+          overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+          font-size: 16px;
+          border: none;
+          outline: none;
+          color: white;
+          padding: 14px 16px;
+          background-color: inherit;
+          font-family: inherit;
+          margin: 0;
+        }
+
+        .navbar a:hover, .dropdown:hover .dropbtn {
+          background-color: red;
+        }
+
+        .dropdown-content {
+          display: none;
+          position: absolute;
+          background-color: #f9f9f9;
+          min-width: 160px;
+          box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+          z-index: 1;
+        }
+
+        .dropdown-content a {
+          float: none;
+          color: black;
+          padding: 12px 16px;
+          text-decoration: none;
+          display: block;
+          text-align: left;
+        }
+
+        .dropdown-content a:hover {
+          background-color: #ddd;
+        }
+
+        .dropdown:hover .dropdown-content {
+          display: block;
+        }
+        </style>
+    </head>
+
+	<body>
+		
+    <div class="navbar">
+            <a href="index.php">Home</a>
+            <div class="dropdown">
+                <button class="dropbtn">View
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="seestudent.php">Student</a>
+                    <a href="seeparent.php">Parent</a>
+                    <a href="viewteacher.php">Teacher</a>
+                    <a href="viewclass.php">Class</a>
+                </div>
+            </div>
+            <div class="dropdown">
+                <button class="dropbtn">Add
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="addstudent.php">Student</a>
+                    <a href="addparent.php">Parent</a>
+                    <a href="addteacher.php">Teacher</a>
+                    <a href="addclass.php">Class</a>
+                </div>
+            </div>
+			<div class="dropdown">
+              <button class="dropbtn">Delete
+                  <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                  <a href="deletestudent.php">Student</a>
+                  <a href="deleteparent.php">Parent</a>
+                  <a href="deleteteacher.php">Teacher</a>
+                  <a href="deleteclass.php">Class</a>
+              </div>
+          </div>
+          <div class="dropdown">
+            <button class="dropbtn">Update
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="updatestudent.php">Student</a>
+                <a href="updateparent.php">Parent</a>
+                <a href="updateteacher.php">Teacher</a>
+                <a href="updateclass.php">Class</a>
+            </div>
+        </div>
+            <a href="parentspupils.php">Parents/Students</a>
+        </div>
+
+		<h2>Welcome to our School Management System</h2>
+		<?php
+		//mysqli_connect() function establishes a connection with MySQL server and returns the connection as an object.
+		$link = mysqli_connect("localhost", "root", "", "school");
+		// Check connection
+		if ($link === false) {
+			die("Connection failed: ");
+		}
+		?>
+	
+		<hr>
+		
+		<h3>See all Teachers</h3>
+	
+		<table>
+		
+			<tr>
+				<th width="150px">Teacher ID<br><hr></th>
+				<th width="250px">Teacher Name<br><hr></th>
+                <th width="250px">TeacherAddress<br><hr></th>
+                <th width="250px">TeacherMedInfo<br><hr></th>
+                <th width="250px">TeacherPhone<br><hr></th>
+                <th width="250px">Salary<br><hr></th>
+                <th width="250px">Background<br><hr></th>
+                <th width="250px">ClassesID<br><hr></th>
+              
+				
+			</tr>
+					
+			<?php
+			$sql = mysqli_query($link, "SELECT TeacherID, TeacherName,TeacherAddress,TeacherMedInfo,TeacherPhone,Salary,Background,ClassesID FROM teacher");
+			while ($row = $sql->fetch_assoc()){
+			echo "
+			<tr>
+				<th>{$row['TeacherID']}</th>
+				<th>{$row['TeacherName']}</th>
+                <th>{$row['TeacherAddress']}</th>
+                <th>{$row['TeacherMedInfo']}</th>
+                <th>{$row['TeacherPhone']}</th>
+                <th>{$row['Salary']}</th>
+                <th>{$row['Background']}</th>
+                <th>{$row['ClassesID']}</th>
+                
+				
+			</tr>";
+			}
+			?>
+			
+		</table>
+		
+		<?php
+		$link->close();
+		?>
+	
+		<hr>
+		
+
+	
+	</body>
+
+</html>
